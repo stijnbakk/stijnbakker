fcc7ed0574804362d34f81e621a5a378958dd3ba
Stijn Bakker <stijn@stijnbakker.com>
2023-05-25 11:26:28 +0200

    add first topics of interest page

---
+++ src/routes/topics-of-interest/+page.svelte
@@ -0,0 +1,24 @@
+<div class=" max-w-3xl py-20 p-4 text-center mx-auto">
+    <h1 class="text-lg font-bold mb-3">Topics of interest</h1>
+    <p class="font-serif text-3xl leading-normal">Some areas I feel have so much potential. But either I do not have the skills to start a venture in it, or I do not possess the time to do so. And sometimes I have a feeling, a hunge, that hasn’t yet blossomed into a key idea or concept. </p>
+</div>
+<div class="w-full p-4 text-center mx-auto">
+    <ul class="linklist">
+        <!-- TODO: make into bouncing bubbles -->
+        <li><a href="/topics-of-interest/better-spreadsheets">Better spreadsheets</a></li>
+        <li><a href="/topics-of-interest/sme-manufacturing">SME manufacturing</a></li>
+        <li><a href="/topics-of-interest/userfriendly-version-control">Userfriendly version control</a></li>
+        <li><a href="/topics-of-interest/end-user-programming">End-user programming</a></li>
+    </ul>
+</div>
+
+<style lang="scss">
+    .linklist {
+        li{
+            @apply text-5xl font-serif underline-offset-2 leading-relaxed
+        }
+        a{
+            @apply hover:underline
+        }
+    }
+</style>
---
+++ src/routes/topics-of-interest/better-spreadsheets/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import SvelteSeo from 'svelte-seo';
+	import Superfeed from '$lib/components/superfeed/Superfeed.svelte';
+	import type { TParsedSuperfeedItem } from '$lib/components/superfeed/types';
+
+	const title = 'Better Spreadsheets';
+
+	type TData = {
+		results: TParsedSuperfeedItem[] | undefined
+	}
+	export let data;
+</script>
+
+
+<div class="">
+	<!-- <h1>Better spreadsheets</h1> -->
+	{#if data.results}
+	<Superfeed items={data.results}/>
+	{/if}
+</div>
