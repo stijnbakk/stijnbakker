215e353cff6b6d66703b07ee7df0a9cd03020ed7
Stijn Bakker <stijn@stijnbakker.com>
2023-06-07 21:12:54 +0200

    og add

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,12 +6,31 @@
 	import Footer from '$lib/components/layout/Footer.svelte';
 	import Header from '$lib/components/layout/Header.svelte';
 
-	const title = 'Stijn Bakker - strategist, concept creator and essayist';
+	const title = 'Stijn Bakker - strategy engineer, business designer and essayist';
 	const description =
 		"I'm a digital strategist, concept creator, essayist and freelance consultant based in Rotterdam";
 </script>
 
-<SvelteSeo title={title} description={description} />
+<SvelteSeo
+	{title}
+	{description}
+	openGraph={{
+		title: title,
+		description: description,
+		images: [
+			{
+				url: 'https://www.stijnbakker.com/og.jpg',
+				width: 1200,
+				height: 630,
+				alt: 'Stijn Bakker'
+			}
+		],
+		url: 'https://www.stijnbakker.com/',
+		type: 'website',
+		site_name: 'Stijn Bakker'
+	}}
+/>
+
 <div class="flex flex-col h-screen justify-between">
 	<Header />
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,20 +4,38 @@
 	import Superfeed from '$lib/components/superfeed/Superfeed.svelte';
 	import type { TParsedSuperfeedItem } from '$lib/components/superfeed/types';
 
-	const title = 'Stijn Bakker - strategist, concept creator and essayist';
+	const title = 'Stijn Bakker - strategy engineer, business designer and essayist';
 	const description =
 		"I'm a digital strategist, concept creator, essayist and freelance consultant based in Rotterdam";
 
 	type TData = {
-		results: TParsedSuperfeedItem[] | undefined
-	}
+		results: TParsedSuperfeedItem[] | undefined;
+	};
 	export let data;
 </script>
 
-<SvelteSeo {title} {description} />
+<SvelteSeo
+	{title}
+	{description}
+	openGraph={{
+		title: title,
+		description: description,
+		images: [
+			{
+				url: 'https://www.stijnbakker.com/og.jpg',
+				width: 1200,
+				height: 630,
+				alt: 'Stijn Bakker'
+			}
+		],
+		url: 'https://www.stijnbakker.com/',
+		type: 'website',
+		site_name: 'Stijn Bakker'
+	}}
+/>
 <div class="">
 	<Hero />
 	{#if data.results}
-	<Superfeed items={data.results}/>
+		<Superfeed items={data.results} />
 	{/if}
 </div>
