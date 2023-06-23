e73ca4fad1af830206cb859cc1bf37624fec8094
Stijn Bakker <stijn@stijnbakker.com>
2023-06-23 15:24:32 +0200

    cleanup

--- src/lib/utils/notion/superfeedNotion.ts
+++
@@ -1,5 +0,0 @@
-import { Client } from '@notionhq/client';
-
-export const superfeedNotion = new Client({
-	auth: import.meta.env.VITE_NOTION_SUPERFEED_SECRET
-});
--- src/routes/newsletter/SignUp.svelte
+++
@@ -1,3 +0,0 @@
-<div class="border border-2">
-	<input type="email" required />
-</div>
