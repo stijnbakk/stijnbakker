477966747be0229ff745d30b7d0261708d029795
Stijn Bakker <stijn@stijnbakker.com>
2023-06-23 15:52:18 +0200

    posts and newsletters show in dev

--- src/routes/writings/+page.server.ts
+++ src/routes/writings/+page.server.ts
@@ -1,9 +1,7 @@
 // import type { PageServerLoad } from '../$types';
 import { slugFromPath } from '$lib/slugFromPath';
 import type { PageServerLoad } from './$types';
-
-// const MAX_POSTS = 10;
-// TODO: Fix sitemap generation for blog - ie. fix static generation for blog
+import { dev } from '$app/environment';
 
 export const load: PageServerLoad = async ({ url }) => {
 	const modules = import.meta.glob(`/src/routes/writings/posts/*.{md,svx,svelte.md}`);
@@ -19,10 +17,9 @@ export const load: PageServerLoad = async ({ url }) => {
 	);
 
 	const posts = await Promise.all(postPromises);
-	const publishedPosts = posts.filter((post) => post.published);
+	const publishedPosts = dev ? posts : posts.filter((post) => post.published);
 
-	// publishedPosts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
-	publishedPosts.sort((a, b) => (new Date(b.date) > new Date(a.date) ? -1 : 1));
+	publishedPosts.sort((a, b) => (new Date(a.date) > new Date(b.date) ? -1 : 1));
 
 	return { posts: publishedPosts };
 };
