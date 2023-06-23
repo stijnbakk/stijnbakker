40c8f37313a65ccb9a08bc7a467b1ad14679101c
Stijn Bakker <stijn@stijnbakker.com>
2023-06-23 20:40:30 +0200

    modified tweetcard to show images

--- src/lib/components/superfeed/TweetCard.svelte
+++ src/lib/components/superfeed/TweetCard.svelte
@@ -5,35 +5,48 @@
 	// export let username: string;
 	// export let tweet: string;
 	// export let imageUrl: string = '';
-	export let item: TParsedSuperfeedItem
+	export let item: TParsedSuperfeedItem;
 
-    const getUserProfileTagWithoutAtSign = (profileTag: string): string=> {
-        return profileTag.startsWith('@')
-				? profileTag.slice(1) // Remove the '@' sign if it exists
-				: profileTag;
-    }
+	const getUserProfileTagWithoutAtSign = (profileTag: string): string => {
+		return profileTag.startsWith('@')
+			? profileTag.slice(1) // Remove the '@' sign if it exists
+			: profileTag;
+	};
 	const getUserProfileUrl = (profileTag: string): string | undefined => {
 		try {
-			const profileWithoutAtSign = getUserProfileTagWithoutAtSign(profileTag)
+			const profileWithoutAtSign = getUserProfileTagWithoutAtSign(profileTag);
 			const twitterUrl = `https://twitter.com/${profileWithoutAtSign}`;
 			return twitterUrl;
 		} catch (error) {
 			return undefined;
 		}
 	};
+
+	// console.log('tweetCard:', item);
 </script>
+
 {#if item?.note && item?.name && item?.url}
-<a href={item?.url}>
-<Card {item}>
-	<div class="mb-8 p-4 mt-3">
-		<p><a href={getUserProfileUrl(item?.note)} class="text-sm font-bold mb-3 hover:underline">@{getUserProfileTagWithoutAtSign(item.note)}</a> • {item.date?.toLocaleDateString()}</p>
-		<p class="mb-3">
-			{item?.name}
-		</p>
-        {#if item?.image && Array.isArray(item?.image)}
-		<!-- <img alt="123" class="w-full aspect-video bg-gray-300 object-cover rounded" src={item?.image}/> -->
-        {/if}
-	</div>
-</Card>
-</a>
+	<a href={item?.url}>
+		<Card {item}>
+			<div class="mb-8 p-4 mt-3 max-w-md">
+				<p>
+					<a href={getUserProfileUrl(item?.note)} class="text-sm font-bold mb-3 hover:underline"
+						>@{getUserProfileTagWithoutAtSign(item.note)}</a
+					>
+					• {item.date?.toLocaleDateString()}
+				</p>
+				<p class="mb-3">
+					{item?.name}
+				</p>
+				{#if item?.image && Array.isArray(item?.image)}
+					{#if item?.image[0]}
+						<img
+							class="w-full aspect-video bg-gray-300 object-cover rounded"
+							src={item?.image[0].url}
+						/>
+					{/if}
+				{/if}
+			</div>
+		</Card>
+	</a>
 {/if}
