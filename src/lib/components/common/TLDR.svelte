f4dd36265b5434a63c67df72ec1c481caa6036a2
Stijn Bakker <stijn@stijnbakker.com>
2023-06-23 18:39:49 +0200

    TLDR component for writings

---
+++ src/lib/components/common/TLDR.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	type TItem = string;
+	export let items: string[];
+</script>
+
+<div class="bg-gray-50 px-8 py-2 rounded mb-10">
+	<p class="font-bold text-lg">TLDR item</p>
+	<ul class="list-outside">
+		{#each items as item}
+			<li>{@html item}</li>
+		{/each}
+	</ul>
+</div>
