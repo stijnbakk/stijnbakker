a83da0d9663c4655896daadce4fdefb1cfa7788f
Stijn Bakker <stijn@stijnbakker.com>
2023-06-29 18:39:43 +0200

    add password

--- src/routes/ventura/+layout.svelte
+++ src/routes/ventura/+layout.svelte
@@ -1,3 +1,20 @@
-<div class=" mx-auto px-4 prose max-w-none">
-	<slot />
-</div>
+<script>
+	let password = '';
+	let passwordSet = 'entrepreneurship';
+</script>
+
+{#if password !== passwordSet}
+	<div class="w-full min-h-screen flex items-center justify-center flex-col gap-4">
+		<p class="font-bold text-sm">Ventura password protected:</p>
+		<input
+			type="password"
+			bind:value={password}
+			placeholder="password"
+			class="text-center rounded"
+		/>
+	</div>
+{:else if password === passwordSet}
+	<div class=" mx-auto px-4 prose max-w-none">
+		<slot />
+	</div>
+{/if}
