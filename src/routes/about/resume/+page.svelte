7a1463ae58f065c1ce9d9661d62a5a2f4dd62d8a
Stijn <stijn@stijnbakker.com>
2023-11-21 10:33:49 +0100

    add resume

---
+++ src/routes/about/resume/+page.svelte
@@ -0,0 +1,144 @@
+<script lang="ts">
+	import SvelteSeo from 'svelte-seo';
+	const title = 'Stijn Bakker - About me';
+	const description =
+		'Stijn Bakker is a digital strategy consultant, product owner and front-end engineer based in Rotterdam, the Netherlands. He has a background in music, engineering and strategy. He is currently working with various corporations, startups and non-profits.';
+</script>
+
+<SvelteSeo {title} {description} nofollow={true} noindex={true} />
+<div class="max-w-prose mx-auto prose px-4">
+	<h1>I'm Stijn Bakker</h1>
+	<div id="intro">
+		<p>
+			Updated november 2023 I’m a digital product executive. I design, strategise, code, brand and
+			manage; leading product teams in creating new businesses and digital products. The more
+			technically complex, the better.
+		</p>
+		<p>
+			I do so now as Lead CoreConnect at RB2, building a platform for SMB companies to drastically
+			simplify the development and maintenance of complex and expensive MACH architectures. In the
+			sidelines I work to setup The Concepting Company, a digital product agency.
+		</p>
+		<p>
+			Cum laude graduated as engineer from TU Delft, self-taught software architect, and experienced
+			across a broad range of industries including corporate operations, tech startups, private
+			equity, e-commerce, boutique consulting and creative agencies.
+		</p>
+	</div>
+	<h2>Personalia</h2>
+	<p>
+		Stijn Bakker<br />
+		Aged 31<br />
+		Based in Rotterdam, NL<br />
+		+31 6 30 51 92 48<br />
+		stijn@stijnbakker.com
+	</p>
+
+	<h2>Professional experience</h2>
+	<div class="mb-3">
+		<h3>Digital Product Director at <i>rb2</i></h3>
+		<i>August 2023 - present</i>
+		<p>
+			As Lead CoreConnect it is my responsibility to develop a completely new digital platform
+			targeting SMB companies to drastically simplify the creation and maintenance of complex MACH
+			architectures. As director I'm responsible for product vision, product development team,
+			investment and go-to market. <br />
+			<span class="text-sm hidden"
+				>references: Dennis van Marle (CEO, rb2), Roelof Blom (CTO, rb2)</span
+			>
+		</p>
+	</div>
+	<div class="mb-3">
+		<h3>Founder at <i>The Concepting Company</i></h3>
+		<i>July 2023 - present</i>
+		<p>
+			A newly setup venture, in collaboration with RB2, with the aim of helping intrapreneurs and
+			entrepreneurs concretise their vision into a coherent product concept, business model and
+			working prototype.
+			<br />
+			<span class="text-sm hidden">references: Mark van der Ploeg (COO, rb2)</span>
+		</p>
+	</div>
+
+	<div class="mb-3">
+		<h3>Independent consultant / interim product manager at <i>Bakkery.io</i></h3>
+		<i>August 2020 - August 2023</i>
+		<p>
+			Shortly after the pandemic broke out, I decided to take the entrepreneurial leap - starting
+			for myself. In these 3 years I took on various roles as business developer, product owner,
+			corporate strategy consultant with various companies in e-commerce, healthcare, digital
+			hosting, digital agencies. <br />
+			<span class="text-sm hidden"
+				>references: Mark van Rooijen (Director M&A, TransIP), Ties Luijendijk (Director Consulting,
+				rb2), Sander Stomph (Managing Partner, KickstartAI), Linda Kester (Owner/managing partner -
+				JigsEye / Nyenrode), Patrick Segaar (Director Strategic HR, Schiphol)</span
+			>
+		</p>
+	</div>
+	<div class="mb-3">
+		<h3>Strategy Consultant / Associate at <i>KLM x Boston Consulting Group</i></h3>
+		<i>November 2017 - August 2020</i>
+		<p>
+			Right-hand to the Vice President Operations Decision Support, I joined the joint venture by
+			KLM and Boston Consulting Group in their first joint hire, to transform airline operations. As
+			consultant I took on various roles including; product ownership for various operational tools,
+			UI/UX design, corporate strategy consultant and secretary of KLM board of innovation.
+			<span class="text-sm hidden">
+				reference: Sander Stomph (VP Operations Decision Support, KLM), Dirk-Maarten Molenaar
+				(Managing Partner, BCG), Marthe van Rooij (VP Digital Product, KLM)</span
+			>
+		</p>
+	</div>
+
+	<h2>Professional experience</h2>
+	<div class="mb-3">
+		<h3>MSc Strategic Innovation Management at <i>Delft University of Technology</i></h3>
+		<i>September 2016 - October 2018</i>
+
+		<ul>
+			<li>Cum laude</li>
+			<li>"MBA of Delft"</li>
+			<li>
+				master programme designed to directly extend a very technical background - Specialised in <i
+					>organisational structures</i
+				>
+				and <i>branding</i>
+			</li>
+			<li>
+				Graduated cum laude at KLM with thesis on managing self-steering digital development teams
+				in a corporate scale-up
+			</li>
+		</ul>
+	</div>
+
+	<div class="mb-3">
+		<h3>Self-taught software architecture/development at <i>Open Source Society University</i></h3>
+		<i> August 2020 - present (spare time)</i>
+		<ul>
+			<li>Self-taught full-stack developer (Node)</li>
+			<li>Proficient software architect in DDD,.NET and Node</li>
+		</ul>
+	</div>
+	<div class="mb-3">
+		<h3>BSc Industrial Design Engineering at <i>Delft University of Technology</i></h3>
+		<i>September 2012 - November 2015</i>
+		<ul>
+			<li>Cum laude</li>
+			<li>
+				Industrial design engineering as a solid base for learning how to do product development
+			</li>
+			<li>
+				Dabbled around with different specialisations into aluminium moulding, surface CAD modelling
+				and (electronic) product architectures
+			</li>
+			<li>
+				Internships & jobs:<br />
+				<ul>
+					<li>2016: 7 month business & branding internship at <i>Pezy Group</i></li>
+					<li>2015: 1 year junior design job at <i>Good Hotel London</i></li>
+					<li>2014: 5 months engineering internship at <i>VanBerlo</i></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</div>
